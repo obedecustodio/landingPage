ee4a5511ccb9f313a2eb939b773987f110180156
obedecossa <obedecossa@outlook.com>
2023-12-19 21:02:21 -0800

    adding the upper navbar

--- translate.js
+++ translate.js
@@ -18,11 +18,32 @@ function switchLanguage() {
     });
 
     currentLanguage = (currentLanguage === 'en') ? 'pt' : 'en';
+
+    if(currentLanguage === 'en'){
+        document.querySelector('#btnEn').classList.remove('d-none')
+        document.querySelector('#btnPt').classList.add('d-none')
+    }else{
+        document.querySelector('#btnPt').classList.remove('d-none')
+        document.querySelector('#btnEn').classList.add('d-none')
+    }
 }
 
-// document.querySelectorAll(".switchLang").forEach(e => e.addEventListener('click',()=>{
-//     switchLanguage()
-//     console.log("translste")
-// } ))
+async function fetchExchangeRates() {
+    const apiKey = '97db30b9fcf9884d141559023806a052';
+    const url = `'https://api.currencyfreaks.com/v2.0/rates/latest?apikey=YOUR_APIKEY'${apiKey}`;
 
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        // Update the displayed exchange rates
+        // document.getElementById('dollarContainer').innerText = `Dollar: ${data.rates.USD}`;
+        // document.getElementById('meticalContainer').innerText = `Metical: ${data.rates.MZN}`;
+    } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+    }
+}
+
+
+fetchExchangeRates();
 setDataPortuguese()
